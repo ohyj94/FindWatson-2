eb96cb53ea67c9ba80f58ef6a666fc1b4af51387
lelode <lys6370@gmail.com>
2019-12-04 14:46:23 +0900

    [sohyun][12-04 PM.12:04] encryption complete (board)

--- src/findwatson/configuration/Configuration.java
+++ src/findwatson/configuration/Configuration.java
@@ -3,4 +3,12 @@ package findwatson.configuration;
 public class Configuration {
 	public static int recordCountPerPage = 10;
 	public static int naviCountPerPage = 10;
+	
+	public static String protectXSS(String input) {
+	      input = input.replaceAll("&", "&amp;");
+	      input = input.replaceAll("<", "&lt;");
+	      input = input.replaceAll(">", "&gt;");
+	      
+	      return input;
+	   }
 }
